1ed315090138f8ff622208ab87bee15500836dd4
Night-Traders-Dev <quegmeister@gmail.com>
2024-12-04 15:07:29 -0500

    init

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 
 #include "pico/stdlib.h"
 
+#define PICO_DEFAULT_LED_PIN 25
 
 static void led_blink (void *paramster)
 {
@@ -27,9 +28,6 @@ static void led_blink (void *paramster)
 
 int main(void)
 {
-    /* Configure the system ready to run the demo.  The clock configuration
-    can be done here if it was not done before main() was called. */
-    /* Want to be able to printf */
     stdio_init_all();
 
     printf("FreeRTOS SMP Template for Raspberry Pi Pico Board\n");
@@ -43,32 +41,37 @@ int main(void)
     /* should never reach here */
     panic_unsupported();
 }
-/*-----------------------------------------------------------*/
+
+/* Static allocation for the Idle task */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize) {
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+    *ppxIdleTaskStackBuffer = xIdleStack;
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+}
+
+/* Static allocation for the Timer task */
+static StaticTask_t xTimerTaskTCBBuffer;
+static StackType_t xTimerStack[configTIMER_TASK_STACK_DEPTH];
+
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize) {
+    *ppxTimerTaskTCBBuffer = &xTimerTaskTCBBuffer;
+    *ppxTimerTaskStackBuffer = xTimerStack;
+    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
+}
+
 
 void vApplicationMallocFailedHook( void )
 {
-    /* The malloc failed hook is enabled by setting
-    configUSE_MALLOC_FAILED_HOOK to 1 in FreeRTOSConfig.h.
-
-    Called if a call to pvPortMalloc() fails because there is insufficient
-    free memory available in the FreeRTOS heap.  pvPortMalloc() is called
-    internally by FreeRTOS API functions that create tasks, queues, software
-    timers, and semaphores.  The size of the FreeRTOS heap is set by the
-    configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
     panic("malloc failed");
 }
-/*-----------------------------------------------------------*/
 
 void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName )
 {
     ( void ) pcTaskName;
     ( void ) xTask;
 
-    /* Run time stack overflow checking is performed if
-    configconfigCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
-    function is called if a stack overflow is detected.  pxCurrentTCB can be
-    inspected in the debugger if the task name passed into this function is
-    corrupt. */
     for( ;; );
 }
-/*-----------------------------------------------------------*/
